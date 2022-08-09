ec05f9978e06ab512c69644dfa54b070a93e39b0
jcasadiego <jcasadiego.a@gmail.com>
2022-08-09 19:04:24 -0400

    create component searcher

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Col } from 'antd';
+import Searcher from './components/Searcher';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Pokedux
+      <Col span={8} offset={8}>
+        <Searcher />
+      </Col>
     </div>
   );
 }
