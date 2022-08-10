15f59f29a877c64531f476d33bcfc65888bf94ac
jcasadiego <jcasadiego.a@gmail.com>
2022-08-10 06:08:09 -0400

    fix pokemon card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Col } from 'antd';
 import Searcher from './components/Searcher';
+import PokemonList from './components/PokemonList';
 import './App.css';
-import PokemonCard from './components/PokemonCard';
+import logo from './statics/logo.svg';
 
 function App() {
   return (
     <div className="App">
+    <Col span={4} offset={10}>
+      <img src={logo} alt="Pokedux"/>
+    </Col>
       <Col span={8} offset={8}>
         <Searcher />
-        <PokemonCard />
       </Col>
+      <PokemonList />
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,14 @@
+import { PlusOutlined } from "@ant-design/icons";
 import { Card } from "antd";
 import Meta from "antd/lib/card/Meta";
+import './PokemonList.css';
 
 const PokemonCard = () => {
     return ( 
     <Card 
         title="Ditto"
-        cover={<img src="" alt="Ditto"/>}
+        cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt="Ditto"/>}
+        extra={<PlusOutlined />}
     >
         <Meta description="Fuego, Magico" />
     </Card>
